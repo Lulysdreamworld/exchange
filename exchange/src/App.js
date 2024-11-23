24e817158e8bd5d572aac0900e036b6d27057410
Lulysdreamworld <Lu.kallweit@gmail.com>
2024-11-23 20:04:46 +0100

    change box-sizing border-box

--- exchange/src/App.js
+++ exchange/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
+import './root.css'
 import StatCard from './StatCard/StatCard';
 import { items } from './items';
 import video1 from './test.mp4'
 
 function App() {
-  console.log(items[1].name)
   return (
     <div className="setup" style={{backgroundColor: `${items[1].color}`}}>
       <div className="main">
+       <video src={video1} width="100%" height="100%" autoPlay muted loop />
       <StatCard 
       name={items[1].name}
       value={items[1].stats.value}
       motivation={items[1].stats.motivation}
       probability={items[1].stats.probability}
       />
-       <video src={video1} width="100%" height="100%" autoPlay muted loop />
       </div>
     </div>
   );
--- exchange/src/items.js
+++ exchange/src/items.js
@@ -7,8 +7,10 @@ export const items = [
         motivation: "Sur",
         probability: "Pla",
       },
-      visual: "/models/sword_of_dawn.glb",
+      description: "“Forged in the ashes of a fallen world, this all-purpose tool is a lifeline to ancient survival. Its blade cuts through wood and bone, its groove strikes fire from stone, and its grip carries the spirit of those who fought to endure. With every use, it reminds its bearer: survival is not just a skill, but a legacy.”",
+      artwork: "/models/sword_of_dawn.glb",
       color: "rgba(255, 255, 255, 1)",
+      passkit: "link to file",
     },
     {
         id: 2,
@@ -18,7 +20,9 @@ export const items = [
           motivation: "Kno",
           probability: "Pla",
         },
-        visual: "/models/sword_of_dawn.glb",
+        description: "“Forged in the ashes of a fallen world, this all-purpose tool is a lifeline to ancient survival. Its blade cuts through wood and bone, its groove strikes fire from stone, and its grip carries the spirit of those who fought to endure. With every use, it reminds its bearer: survival is not just a skill, but a legacy.”",
+        artwork: "/models/sword_of_dawn.glb",
         color: "rgba(107, 76, 10, 1)",
+        passkit: "link to file",
       },
   ];
